83be622ad405b7b549dd0fe7b2837081aa68d9c8
iliass-de <iliasshlm@gmail.com>
2023-02-14 17:35:21 +0100

    refactor: add new field (AvailableQuantity)

--- src/Models/Product.cs
+++ src/Models/Product.cs
@@ -6,6 +6,6 @@ public class Product {
     public string Name { get; set; }
     public string Description { get; set; }
     public double Price { get; set; }
-    public SellingStatus Status { get; set; }
+    public int AvailableQuantity { get; set; }
 
 }
