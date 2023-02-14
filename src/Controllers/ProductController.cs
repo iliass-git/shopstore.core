3dd77fb6c3d462eb138f677aa38ee3feefb4e9ad
iliass-de <iliasshlm@gmail.com>
2023-02-14 14:44:42 +0100

    feat: add product controller

---
+++ src/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using ShopStore.Interfaces;
+using ShopStore.Models;
+
+namespace ShopStore.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly ILogger<ProductController> _logger;
+    private readonly IProductRepository _productRepository;
+    public ProductController(IProductRepository productRepository, ILogger<ProductController> logger )
+    {
+     _logger = logger;   
+     _productRepository = productRepository;
+    }
+
+
+    [HttpPost ,Route("/AddProduct")]
+    public void AddProduct(Product product)
+    {
+        _productRepository.AddProduct(product);
+    }
+}
