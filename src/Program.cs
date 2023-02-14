d1ca700601db6e3c76c16d711ceb2517bdbc6e60
iliass-de <iliasshlm@gmail.com>
2023-02-14 14:45:16 +0100

    feat: register the product and dbContext services

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,17 @@
+using ShopStore.Interfaces;
+using ShopStore.Repository;
+using  ShopStore.DbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+ builder.Services.AddDbContext<DataContext>();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
