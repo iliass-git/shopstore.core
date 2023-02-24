45ce8a7709e4960844fbd6ec20d0008db07afb59
iliass-de <iliasshlm@gmail.com>
2023-02-25 00:16:49 +0100

    feat: add userInfos entity

---
+++ src/Migrations/20230224231539_AddUserInfos.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ShopStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserInfos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Customers",
+                newName: "Mobile");
+
+            migrationBuilder.RenameColumn(
+                name: "RegistrationDate",
+                table: "Customers",
+                newName: "BirthDate");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Customers",
+                newName: "FixedLine");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserInfoId",
+                table: "Customers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_UserInfoId",
+                table: "Customers",
+                column: "UserInfoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_UserInfo_UserInfoId",
+                table: "Customers",
+                column: "UserInfoId",
+                principalTable: "UserInfo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_UserInfo_UserInfoId",
+                table: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_UserInfoId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UserInfoId",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "Mobile",
+                table: "Customers",
+                newName: "UserName");
+
+            migrationBuilder.RenameColumn(
+                name: "FixedLine",
+                table: "Customers",
+                newName: "PasswordHash");
+
+            migrationBuilder.RenameColumn(
+                name: "BirthDate",
+                table: "Customers",
+                newName: "RegistrationDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- src/Models/Customer.cs
+++ src/Models/Customer.cs
@@ -5,10 +5,10 @@ public class Customer {
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string UserName { get; set; }
-    public string PasswordHash { get; set; }
-    public string Email { get; set; }
+    public DateTime BirthDate { get; set; }
     public string Address { get; set; }
-    public DateTime RegistrationDate { get; set; }
-
+    public string Mobile { get; set; }
+    public string FixedLine { get; set; }
+    public UserInfo UserInfo { get; set; }
 }
+
