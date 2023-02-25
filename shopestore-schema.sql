18fccb0d869f2495af5f6c71d32c03020b100308
iliass-de <iliasshlm@gmail.com>
2023-02-26 00:17:41 +0100

    fix: update sql file with create database statement

--- shopestore-schema.sql
+++ shopestore-schema.sql
@@ -5,7 +5,30 @@
 -- Dumped from database version 15.2 (Debian 15.2-1.pgdg110+1)
 -- Dumped by pg_dump version 15.1
 
--- Started on 2023-02-25 18:06:28 UTC
+-- Started on 2023-02-25 23:15:46 UTC
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 3360 (class 1262 OID 16389)
+-- Name: shop-store; Type: DATABASE; Schema: -; Owner: server_admin
+--
+
+CREATE DATABASE "shop-store" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE "shop-store" OWNER TO server_admin;
+
+\connect -reuse-previous=on "dbname='shop-store'"
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -261,7 +284,7 @@ ALTER TABLE ONLY public."Customers"
     ADD CONSTRAINT "FK_Customers_UserInfos_UserInfoId" FOREIGN KEY ("UserInfoId") REFERENCES public."UserInfos"("Id") ON DELETE CASCADE;
 
 
--- Completed on 2023-02-25 18:06:28 UTC
+-- Completed on 2023-02-25 23:15:46 UTC
 
 --
 -- PostgreSQL database dump complete
